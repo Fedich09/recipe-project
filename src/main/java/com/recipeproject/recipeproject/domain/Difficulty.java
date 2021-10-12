877947b8304269fceaf91db99cc96c5f9a753d7b
mfedenko <mfedenko@office.atlassian.com>
2021-10-12 21:30:26 +0300

    added enum class and marked this class like enumerated in Recipe class

---
+++ src/main/java/com/recipeproject/recipeproject/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.recipeproject.recipeproject.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
--- src/main/java/com/recipeproject/recipeproject/domain/Recipe.java
+++ src/main/java/com/recipeproject/recipeproject/domain/Recipe.java
@@ -3,6 +3,8 @@ package com.recipeproject.recipeproject.domain;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -24,10 +26,10 @@ public class Recipe {
     private String source;
     private String url;
     private String direction;
-    //todo add
-    //private Difficulty difficulty;
     @Lob
     private Byte[] image;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
@@ -104,5 +106,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
