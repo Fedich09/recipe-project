2f20ab33ac428772a4ab3df45014f4f9fb365be3
mfedenko <mfedenko@office.atlassian.com>
2021-10-13 23:20:11 +0300

    added findByDescription in CategoryRepository and UnitOfMeasureRepository

--- src/main/java/com/recipeproject/recipeproject/repositories/CategoryRepository.java
+++ src/main/java/com/recipeproject/recipeproject/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.recipeproject.recipeproject.repositories;
 import com.recipeproject.recipeproject.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
