a63386bab9a8321569ec0e3dca5c2fda257cfb7d
mfedenko <mfedenko@office.atlassian.com>
2021-10-14 21:12:32 +0300

    did refactor, now bi-directional relationship maneged in on class - Recipe

--- src/main/java/com/recipeproject/recipeproject/bootstrap/DataBootstrap.java
+++ src/main/java/com/recipeproject/recipeproject/bootstrap/DataBootstrap.java
@@ -43,26 +43,25 @@ public class DataBootstrap implements ApplicationListener<ContextRefreshedEvent>
 
         Ingredient avocado = new Ingredient();
         avocado.setAmount(BigDecimal.valueOf(2));
-        avocado.setRecipe(theBestGuacamole);
         avocado.setUnitOfMeasure(unitOfMeasureRepository.findByDescription("Ripe").orElseThrow());
+
         Ingredient salt = new Ingredient();
         salt.setUnitOfMeasure(unitOfMeasureRepository.findByDescription("Teaspoon").orElseThrow());
         salt.setAmount(BigDecimal.valueOf(0.25));
-        salt.setRecipe(theBestGuacamole);
+
+
         Ingredient freshLime = new Ingredient();
         freshLime.setAmount(BigDecimal.valueOf(1));
-        freshLime.setRecipe(theBestGuacamole);
         freshLime.setUnitOfMeasure(unitOfMeasureRepository.findByDescription("Tablespoon").orElseThrow());
 
-        theBestGuacamole.getIngredients().add(avocado);
-        theBestGuacamole.getIngredients().add(salt);
-        theBestGuacamole.getIngredients().add(freshLime);
+        theBestGuacamole.addIngredient(avocado);
+        theBestGuacamole.addIngredient(salt);
+        theBestGuacamole.addIngredient(freshLime);
 
         Notes notes = new Notes();
         notes.setRecipeNotes("Be careful handling chilis! If using, it's best to wear food-safe gloves." +
                 " If no gloves are available, wash your hands thoroughly after handling, " +
                 "and do not touch your eyes or the area near your eyes for several hours afterwards.");
-        notes.setRecipe(theBestGuacamole);
 
         theBestGuacamole.setNotes(notes);
 
--- src/main/java/com/recipeproject/recipeproject/domain/Recipe.java
+++ src/main/java/com/recipeproject/recipeproject/domain/Recipe.java
@@ -114,6 +114,7 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
     public Difficulty getDifficulty() {
@@ -122,6 +123,12 @@ public class Recipe {
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public Recipe addIngredient(Ingredient ingredient) {
+        ingredient.setRecipe(this);
+        this.getIngredients().add(ingredient);
+        return this;
     }
 
     public Set<Ingredient> getIngredients() {
