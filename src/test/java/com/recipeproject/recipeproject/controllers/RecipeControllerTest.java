6eeb5d4428d5f129ca3c896bcfb326e58f121bf2
mfedenko <mfedenko@office.atlassian.com>
2021-11-24 20:36:56 +0200

    add RecipeController, RecipeControllerTest and update RecipeService

---
+++ src/test/java/com/recipeproject/recipeproject/controllers/RecipeControllerTest.java
@@ -0,0 +1,47 @@
+package com.recipeproject.recipeproject.controllers;
+
+import com.recipeproject.recipeproject.domain.Recipe;
+import com.recipeproject.recipeproject.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(MockitoExtension.class)
+class RecipeControllerTest {
+    @Mock
+    private RecipeService recipeService;
+
+    private RecipeController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new RecipeController(recipeService);
+    }
+
+    @Test
+    void getRecipe() throws Exception {
+        //GIVEN
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        // WHEN
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        //THEN
+        mockMvc.perform(MockMvcRequestBuilders.get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+    }
+}
